bf207b91a3afe73ae3552de76dcc80962aadbd15
Vinayak Khade <67099367+vinayak0x01@users.noreply.github.com>
2024-09-09 09:14:37 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountID = 144553
+let accountEmail = "vkhad383@gmail.com"
+var acoountPassword = "123456"
+accountcity = "Pune"
+let accountState
+
+accountEmail = "abc@gmail.com"
+accountPassword = "29292929"
+accountCity = "Talegaon Dabhade"
+
+console.table([accountID, accountEmail, accountPassword, accountPassword, accountCity, accountState])
